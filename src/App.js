811f47e731eda6ae3fba0f3e9d3d1eb94153dd1f
Roberto Castanon <robertocastanon925@gmail.com>
2019-07-19 16:37:16 -0700

    added comments and changed image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ImgCard from './components/Img'
 import characters from './characters.json'
 import Footer from './components/Footer'
 
-//shuffle upon each click
+// this function shuffles when a card is clicked
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -16,32 +16,38 @@ function shuffle(array) {
 }
 
 class App extends Component {
+  // set initial variables to use later on to 0 or empty
   state = {
     characters,
+    // set scores to zero
     score: 0,
-    topScore: 0,
+    highScore: 0,
     beenClicked: [],
+    // at 0 these alerts will not display
     displayLoss: 0,
     displayWin: 0
 
   }
-
+  // clicked function
   clicked = id => {
+    // will asign the state of the empty beenClicked array to be updated
     let beenClicked = this.state.beenClicked
     let score = this.state.score
-    let topScore = this.state.topScore
+    let highScore = this.state.highScore
     this.setState({
       displayLoss: 0
     })
-  
+  // if the the clicked card has an ID of the already indexed characters
   if (beenClicked.indexOf(id) === -1) {
+    // push into the beenClicked array to be stored
     beenClicked.push(id)
     console.log(beenClicked)
-
-    this.handleIncrement()
+    // run function  to handle the socre
+    this.handleScore()
+    // run function to shuffle after every click
     this.createShuffle()
-
   } else if (this.state.score === 12) {
+    // then display the win alert, if the score equals to 12
     this.setState({
       displayWin: 1,
       score: 0,
@@ -49,6 +55,7 @@ class App extends Component {
     })
   } else {
     this.setState({
+      // else then display loss alert
       score: 0,
       beenClicked: []
     })
@@ -57,18 +64,19 @@ class App extends Component {
       displayLoss: 1
     })
   }
-
-  if (score > topScore) {
+  // if the score is larger than the highScore than set highScore to score
+  if (score > highScore) {
     this.setState({
-      topScore: score
+      highScore: score
     })
   }
 }
-
-handleIncrement = () => {
+// function to handle the increase of score after every click that isnt a dupelicate
+handleScore = () => {
   this.setState({ score: this.state.score + 1 })
 }
 
+// functino to shuffle up all the cards
 createShuffle = () => {
   this.setState({ characters: shuffle(characters) })
 }
@@ -79,7 +87,7 @@ createShuffle = () => {
         <ScoreNav 
           score={this.state.score}
           title="This is a title"
-          topScore={this.state.topScore}
+          highScore={this.state.highScore}
         />
         <row>
         <div className="alert alert-danger text-center" style={{ opacity: this.state.displayLoss }}>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ function ScoreNav(props) {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="col-sm"><h5>Current Score: {props.score} </h5></div>
       <div className="col-sm"><h4>{props.title}</h4></div>
-      <div><h5>High Score: {props.topScore}</h5></div>
+      <div><h5>High Score: {props.highScore}</h5></div>
     </nav>
   )
 }
