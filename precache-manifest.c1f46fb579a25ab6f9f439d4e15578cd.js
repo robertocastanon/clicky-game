f30a0bf241176b6ad27f659940c6f8bf6203723e
Roberto Castanon <robertocastanon925@gmail.com>
2019-07-20 20:25:53 -0700

    Updates

---
+++ precache-manifest.c1f46fb579a25ab6f9f439d4e15578cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37ad5377dd6552cacbbed28aafa364d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "19e1ee0b74bf2ad59d4a",
+    "url": "/clicky-game/static/css/main.98f73911.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "19e1ee0b74bf2ad59d4a",
+    "url": "/clicky-game/static/js/main.50b44dd5.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "fc473a31984937eec96138dfd0c0d1ef",
+    "url": "/clicky-game/static/media/bb-wallpaper.fc473a31.png"
+  }
+]);
