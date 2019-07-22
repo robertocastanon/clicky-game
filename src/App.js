5cb838016cd5ef1e6f655577a4477c86333b0dd6
Roberto Castanon <robertocastanon925@gmail.com>
2019-07-21 23:45:59 -0700

    changed theme of the clicky game

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ createShuffle = () => {
       <div>
         <ScoreNav 
           score={this.state.score}
-          title="BB Game"
+          title="Battle Cats!"
           highScore={this.state.highScore}
         />
         <row>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 
 const FooterComp = props => (
-  <nav className="navbar navbar-expand-lg bg-dark text-light">
+  <nav className="navbar navbar-expand-lg bg-warning text-light">
     <a href="https://github.com/robertocastanon/clicky-game" className="col-sm text-center"><h7>Github Repo</h7></a>
   </nav>
 )
